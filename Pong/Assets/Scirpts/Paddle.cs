b8b22bea9afe0ef49466643f5436d01ef0b4bd4b
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-07-13 12:08:40 +0300

    Player paddle logic

---
+++ Pong/Assets/Scirpts/Paddle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField]
+    protected float Speed = 10f;
+    protected Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Pong/Assets/Scirpts/PlayerPaddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddle : Paddle
+{
+    private Vector2 _direction;
+
+    private void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else _direction = Vector2.zero;
+    }
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        if(_direction.sqrMagnitude != 0)
+        {
+            _rb.AddForce(_direction * Speed);
+        }
+    }
+}
