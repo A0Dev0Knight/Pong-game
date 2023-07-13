a91e1df92ecf44dadd6e972c2c4545b3020be9bd
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-07-13 15:49:57 +0300

    Main menu
    added main menu
    
    needs buttons
    functionality

---
+++ Pong/Assets/Scirpts/PaddleBot2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBot2 : Paddle
+{
+    [SerializeField]
+    private Rigidbody2D Ball;
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Ball.velocity.x < 0f)
+        {
+            if (Ball.position.y > this.transform.position.y)
+                _rb.AddForce(Vector2.up * Speed);
+            else _rb.AddForce(Vector2.down * Speed);
+        }
+    }
+}
