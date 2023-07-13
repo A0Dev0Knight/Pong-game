7b99a2544b7e8025c2f399a2dfc048880a0d800f
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-07-13 20:37:17 +0300

    Main Menu up and running

---
+++ Pong/Assets/Scirpts/MainMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    int _ScoreBot1;
+    int _ScoreBot2;
+
+    [SerializeField]
+    Ball Ball;
+
+    [SerializeField]
+    Text Bot1ScoreText;
+
+    [SerializeField]
+    Text Bot2ScoreText;
+
+    [SerializeField]
+    Paddle Bot1Paddle;
+
+    [SerializeField]
+    Paddle Bot2Paddle;
+
+    private void ResetRound()
+    {
+        Ball.ResetBallPoz();
+        Bot1Paddle.ResetPoz();
+        Bot2Paddle.ResetPoz();
+        Ball.AddStartingForce();
+    }
+    public void Bot1Scored()
+    {
+        _ScoreBot1++;
+        Bot1ScoreText.text = _ScoreBot1.ToString();
+        ResetRound();
+    }
+    public void Bot2Scored()
+    {
+        _ScoreBot2++;
+        Bot2ScoreText.text = _ScoreBot2.ToString();
+        ResetRound();
+    }
+}
