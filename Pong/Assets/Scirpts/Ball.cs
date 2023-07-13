d9f7e3d7934ded295f3ca8d64eaaf1619f99d1a7
AlexDaniel-nice <112327676+A0Dev0Knight@users.noreply.github.com>
2023-07-13 12:30:35 +0300

    Ball logic

---
+++ Pong/Assets/Scirpts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 200.0f;
+
+    private Rigidbody2D _rbBall;
+    private void Awake()
+    {
+        _rbBall = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        AddStartingForce();
+    }
+
+    private void AddStartingForce()
+    {
+        float x = UnityEngine.Random.value < .5f ? -1.0f : 1.0f;
+        float y = UnityEngine.Random.value < .5f ? UnityEngine.Random.Range(-1.0f, -.5f) :
+                                                   UnityEngine.Random.Range(.5f, 1.0f);
+        Vector2 _ballDirection = new Vector2(x, y);
+        _rbBall.AddForce(_ballDirection* Speed);    
+    }
+}
